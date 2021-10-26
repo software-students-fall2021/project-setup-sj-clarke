da946be852dc4177501dc6e2074dbddba07d8be0
Danielle Zhao <danielle.hope.zhao@gmail.com>
2021-10-26 00:42:05 -0400

    add expandable button to All Groups

--- front-end/src/allGroups.js
+++ front-end/src/allGroups.js
@@ -1,42 +1,115 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './allGroups.css'
 import * as ReactBootStrap from "react-bootstrap";
 import Title from './header.js'
+import data from "./mockGroups.json"
+import groupExpand from "./groupExpand.js"
 
 function AllGroups(){
+    
+    // Data in table that will be used and can be updated dynamically
+    const [groups, setGroups] = useState(data)
 
-     const groups =  [
+    // State variable to keep track of all the expanded rows
+    // By default, nothing expanded. Hence initialized with empty array.
+    const [expandedRows, setExpandedRows] = useState([]);
+
+    // State variable to keep track which row is currently expanded.
+    const [expandState, setExpandState] = useState({});
+    
+    /**
+     * This function gets called when show/hide link is clicked.
+    */
+    const handleExpandRow = (event, userId) => {
+      const currentExpandedRows = expandedRows;
+      const isRowExpanded = currentExpandedRows.includes(userId);
   
-        {date: "Sep 2021", trip:  "Mexico"},
-        {date: "Aug 2019", trip:  "Paris"}
-        ]
-        
-    // creating a row for each instance within JSON file holding all of the transactions
-    const renderRow = (group, index) => {
-        return (
-            <tr key = {index}>
-              <td>{group.date}</td>
-              <td>{group.trip}</td>
-            </tr>
-            )
+      let obj = {};
+      isRowExpanded ? (obj[userId] = false) :  (obj[userId] = true);
+      setExpandState(obj);
+  
+      // If the row is expanded, we are here to hide it. Hence remove
+      // it from the state variable. Otherwise add to it.
+      const newExpandedRows = isRowExpanded ?
+            currentExpandedRows.filter(id => id !== userId) :
+            currentExpandedRows.concat(userId);
+  
+      setExpandedRows(newExpandedRows);
     }
     return (
-        <div className= "AllGroups">
-        <Title/>
-        <title className ="Main">All Groups
-        </title>  
-        <ReactBootStrap.Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Group</th>
-              </tr>
+      <Container>
+      <Row>
+        <Col>
+          <h1> All Groups({ data.length })</h1>
+        </Col>
+       </Row>
+       <div className= "AllGroups">
+       <ReactBootStrap.Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Group</th>
+            </tr>
           </thead>
           <tbody>
-            {group.map(renderRow)}
+            {groups.map(()=> group)}
+            <tr>
+              <td>{groups.date}</td>
+              <td>{groups.groupName}</td>
+              <td>
+                <Button 
+                  variant="link"
+                  onClick={event => handleExpandRow(event, groups.id)}>
+                    {
+                      expandState[user.id] ?
+                        'Hide' : 'Show'
+                    }
+                 </Button>
+               </td>
+            </tr>
+            {/* {
+               expandedRows.includes(user.id) ?
+                  <tr>
+                    <td colspan="6">
+                      <div style={{backgroundColor: '#343A40', color: '#FFF', padding: '10px'}}>
+                        <h2> Details </h2>
+                        <ul>
+                          <li>
+                            <span><b>Full Name:</b></span> {' '}
+                            <span> { user['first_name'] } {' '} { user['last_name'] } </span>
+                          </li>
+                          <li>
+                            <span><b>Company:</b></span> {' '}
+                            <span> { user.company } </span>
+                          </li>
+                          <li>
+                            <span><b>Department:</b></span> {' '}
+                            <span> { user.department } </span>
+                          </li>
+                          <li>
+                            <span><b>Ip:</b></span> {' '}
+                            <span> { user['ip_address'] } </span>
+                          </li>
+                          <li>
+                            <span><b>Best Movie:</b></span> {' '}
+                            <span> { user.movies } </span>
+                          </li>
+                          <li>
+                            <span><b>About:</b></span> {' '}
+                            <span> { user.about } </span>
+                          </li>
+                        </ul>
+                      </div>
+                    </td>
+                  </tr> : null
+                }
+                </>
+              </> 
+              )} */}
           </tbody>
         </ReactBootStrap.Table>
       </div>
+      </Container>
 
     )
 }
