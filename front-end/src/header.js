8ccac6dfe969f56363eb4a7e837f91c1b638da72
Gal Ben-Shushan <gbs278@nyu.edu>
2021-10-28 15:03:34 -0400

    fixed warnings and and added prettier formatting

--- front-end/src/header.js
+++ front-end/src/header.js
@@ -1,48 +1,72 @@
+import "./header.css";
 
-import './header.css';
-
-import React from 'react'
-import {useState} from 'react'
-import * as ReactBootStrap from "react-bootstrap"; 
-import  ExpenseAction from './addExpenseModal';
-import SettleUp from './settleUpModal';
-import './friends.js';
-import Friends from './friends.js'
-import './accountInfo.js'
+import React from "react";
+import { useState } from "react";
+import * as ReactBootStrap from "react-bootstrap";
+import ExpenseAction from "./addExpenseModal";
+import SettleUp from "./settleUpModal";
+import "./friends.js";
+import "./accountInfo.js";
 
 function Title() {
   const [showAddExpense, setAddExpense] = useState(false);
   const [showSettleUp, setSettleUp] = useState(false);
   return (
-      <div className="Title">
-      <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="medium" variant="light" href="/#home">
-  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="me-auto">
-      <ReactBootStrap.Nav.Link href="/CreateGroup">Create A Group</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="/allGroups">All Groups</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="/Friends">Friends</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.NavDropdown title="Current Group" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="#action/AddExpense" onClick={() => setAddExpense(true)}>Add Expense</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/SettleUp" onClick={() => setSettleUp(true)}>  Settle up</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="/CurrentGroupMembers">Group members</ReactBootStrap.NavDropdown.Item>
-      </ReactBootStrap.NavDropdown>
-    <ReactBootStrap.Nav.Link href="/Account">Account</ReactBootStrap.Nav.Link>
-    </ReactBootStrap.Nav>
-    <ReactBootStrap.Nav>
-    </ReactBootStrap.Nav>
-  </ReactBootStrap.Navbar.Collapse>
-  <ReactBootStrap.Navbar.Brand href="/">TripSplit</ReactBootStrap.Navbar.Brand>
-</ReactBootStrap.Navbar>
-<ExpenseAction show={showAddExpense} setModal={setAddExpense}/>
-<SettleUp show={showSettleUp} setModal={setSettleUp}/>
-
-
-
-
-
+    <div className="Title">
+      <ReactBootStrap.Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="medium"
+        variant="light"
+        href="/#home"
+      >
+        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+          <ReactBootStrap.Nav className="me-auto">
+            <ReactBootStrap.Nav.Link href="/CreateGroup">
+              Create A Group
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="/allGroups">
+              All Groups
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="/Friends">
+              Friends
+            </ReactBootStrap.Nav.Link>
+            <ReactBootStrap.NavDropdown
+              title="Current Group"
+              id="collasible-nav-dropdown"
+            >
+              <ReactBootStrap.NavDropdown.Item
+                href="#action/AddExpense"
+                onClick={() => setAddExpense(true)}
+              >
+                Add Expense
+              </ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item
+                href="#action/SettleUp"
+                onClick={() => setSettleUp(true)}
+              >
+                {" "}
+                Settle up
+              </ReactBootStrap.NavDropdown.Item>
+              <ReactBootStrap.NavDropdown.Item href="/CurrentGroupMembers">
+                Group members
+              </ReactBootStrap.NavDropdown.Item>
+            </ReactBootStrap.NavDropdown>
+            <ReactBootStrap.Nav.Link href="/Account">
+              Account
+            </ReactBootStrap.Nav.Link>
+          </ReactBootStrap.Nav>
+          <ReactBootStrap.Nav></ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+        <ReactBootStrap.Navbar.Brand href="/">
+          TripSplit
+        </ReactBootStrap.Navbar.Brand>
+      </ReactBootStrap.Navbar>
+      <ExpenseAction show={showAddExpense} setModal={setAddExpense} />
+      <SettleUp show={showSettleUp} setModal={setSettleUp} />
     </div>
-  )
+  );
 }
 
-export default Title
+export default Title;
