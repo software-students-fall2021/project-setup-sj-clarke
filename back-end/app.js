e89d3c9cec916d67acd722fd5f60a55e9722cafb
eh2376 <eh2376@nyu.edu>
2021-11-17 14:24:35 -0500

    package.json

--- back-end/app.js
+++ back-end/app.js
@@ -2,10 +2,8 @@
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
 const axios = require('axios')
-<<<<<<< HEAD
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-=======
 // connection to mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://tripsplit:tripsplit123@tripsplit.5k1jw.mongodb.net/TripSplit?retryWrites=true&w=majority'); 
@@ -62,7 +60,6 @@ const group = mongoose.model('group', group_schema)
 
  // group_practice.save().then(() => console.log("POSTED GROUP")); 
 
->>>>>>> 51aa93b3ce499a11c99ed3e102f0ba02b32889e7
 // Middleware 
 app.use(express.json()) // decode JSON-formatted incoming POST data
 app.use(morgan('dev'))
