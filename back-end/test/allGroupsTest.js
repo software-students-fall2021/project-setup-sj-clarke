24814f127c527e10709529d66978f2b05c8d3a6d
Danielle Zhao <danielle.hope.zhao@gmail.com>
2021-12-03 13:54:42 -0500

    AllGroups testing

--- back-end/test/allGroupsTest.js
+++ back-end/test/allGroupsTest.js
@@ -1,10 +1,22 @@
-// process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
 
-// const expect = require("chai").expect; 
-// const request = require("supertest")
-// const app = require("../app.js");
-// const { response } = require("express");
+const expect = require("chai").expect; 
+const request = require("supertest")
+const app = require("../app.js");
+const { response } = require("express");
 
+describe('GET /AllGroups/:username ', () => {
+    it('PASS, getting all friends of a user', (done) => {
+      const user = "sjclarke"
+      request(app).get(`/AllGroups/${user}`)
+        .then((res) => {
+          const body = res.body;
+          expect(res.body).to.be.an("array")
+          done();
+        })
+        .catch((err) => done(err));
+    });
+  })
 // // test GET for All Groups
 // // Testing to see that the mockaroo sends back 10 groups
 // describe('GET /AllGroups ', () => {
@@ -19,4 +31,3 @@
 //         .catch((err) => done(err));
 //     });
 //   })
-   
