bd19ae6a75e66286824b523468f07416ff42f6e6
eh2376 <eh2376@nyu.edu>
2021-12-05 22:42:43 -0500

    adding env file

--- back-end/test/createGroupTest.js
+++ back-end/test/createGroupTest.js
@@ -1,10 +1,5 @@
 
-/*process.env.NODE_ENV = 'test';
 
-// const expect = require("chai").expect; 
-// const request = require("supertest")
-// const app = require("../app.js");
-// const { response } = require("express");
 
 
 // // test GET 
