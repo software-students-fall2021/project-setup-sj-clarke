890bb12dc15e2a2249082a8412ba2c5838f2d62c
Elizabeth Jiranek <elizabethjiranek@ElizabethsMBP2.lan>
2021-12-06 04:03:04 -0500

    updating account info page

--- front-end/src/accountInfo.js
+++ front-end/src/accountInfo.js
@@ -4,6 +4,7 @@ import {useState, useEffect} from 'react'
 import axios from 'axios'
 import "./mockUsers.json"
 import {Link} from 'react-router-dom'
+import { set } from 'js-cookie'
 
 
 
@@ -11,27 +12,49 @@ function Account(props){
     const jwtToken = localStorage.getItem("token")
     console.log(`JWT token: ${jwtToken}`)
     const [isLoggedIn, setIsLoggedIn] = useState(jwtToken && true)
+    const [currentUser, setCurrentUser] = useState();
+    const username = process.env.REACT_APP_USERNAME;
+    const [accountInfo, setAccountInfo] = useState([]);
+    const [fName, setFName] = useState()
+    const [lName, setLName] = useState()
+    const [uName, setUName] = useState()
+   
+
+    useEffect(() => {
+        async function fetchData (){
+            const response = await axios(`/accountinfo/${username}`, { headers: { Authorization: `JWT ${jwtToken}` } });
+
+            setFName(response.data.fName)
+            setLName(response.data.lName)
+            setUName(response.data.username);
+        
+        }
+        fetchData();
+    }, []);
 
 
+ 
     
 
-    
-
-  
 
     
     return(
+       
         <>
         {isLoggedIn ? (
         <div className="Account">
             <header>Account Information</header>
-            <p>Change your account information</p>
+            <p>View your account information</p>
             <form>
-                <label>First Name<input type="text" name="fName"></input></label>
-                <label>Last Name<input type="text" name="lName" ></input></label>
-                <label>Username<input type="text" name="username" ></input></label>
-                <label>Password<input type="password" name="password"></input></label>
-                <input type="submit" value="Change Information" />
+                <label>First Name<input
+                id="fName"
+                type="text" 
+                name="fName" 
+                value={fName}
+                disabled="disabled"
+                ></input></label>
+                <label>Last Name<input type="text" name="lName" value={lName} disabled="disabled"></input></label>
+                <label>Username<input type="text" name="username" value={uName} disabled="disabled" ></input></label>
 
             </form>
 
