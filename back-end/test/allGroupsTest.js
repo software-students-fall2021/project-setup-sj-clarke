435d9a1173fc0cb44d1e2e40710a50a985a085b6
Danielle Zhao <danielle.hope.zhao@gmail.com>
2021-12-05 22:15:14 -0500

    All Groups test

--- back-end/test/allGroupsTest.js
+++ back-end/test/allGroupsTest.js
@@ -1,10 +1,13 @@
 process.env.NODE_ENV = 'test';
 
+const chai = require('chai')
 const expect = require("chai").expect; 
 const request = require("supertest")
 const app = require("../app.js");
 const { response } = require("express");
+let mongoose = require("mongoose");
 
+  
 describe('GET /AllGroups/:username ', () => {
     it('PASS, getting all friends of a user', (done) => {
       const user = "sjclarke"
